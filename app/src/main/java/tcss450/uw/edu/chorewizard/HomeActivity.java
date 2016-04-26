0d67e5c3b36e5392193819f696d002f7a5ce66ab
Alice Clanton <alicepholden@hotmail.com>
2016-04-26 11:26:54 -0700

    Added code into HomeActivity

--- app/src/main/java/tcss450/uw/edu/chorewizard/HomeActivity.java
+++ app/src/main/java/tcss450/uw/edu/chorewizard/HomeActivity.java
@@ -1,11 +1,14 @@
 package tcss450.uw.edu.chorewizard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,15 +18,12 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    // Takes user to AddMemberActivity
+    public void clickAddMember(View view) {
+        Intent intent = new Intent(this, AddMemberActivity.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/tcss450/uw/edu/chorewizard/WelcomeActivity.java
+++ app/src/main/java/tcss450/uw/edu/chorewizard/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package tcss450.uw.edu.chorewizard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    // Takes user to HomeActivity
+    public void clickOk(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
