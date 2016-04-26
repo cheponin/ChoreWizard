9493356fd6d741587a3a1410184db8a1d0e86176
cheponin <nina.chepovska@gmail.com>
2016-04-26 11:25:34 -0700

    Added UI elements to AddMemberActivity

--- app/src/main/java/tcss450/uw/edu/chorewizard/AddMemberActivity.java
+++ app/src/main/java/tcss450/uw/edu/chorewizard/AddMemberActivity.java
@@ -1,11 +1,16 @@
 package tcss450.uw.edu.chorewizard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddMemberActivity extends AppCompatActivity {
+
+    private String mMemberName;
+    private String mMemberPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,19 @@ public class AddMemberActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+
     }
+
+    public void saveMemberInfo(View view) {
+        EditText nameField = (EditText) findViewById(R.id.Member_Name);
+        mMemberName = nameField.getText().toString();
+
+        EditText phoneField = (EditText) findViewById(R.id.Member_Phone);
+        mMemberPhone = phoneField.getText().toString();
+
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
 
 }
