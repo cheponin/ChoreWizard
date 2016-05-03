b731f3f3cfc33a70278e40849f063f95b60dbac8
cheponin <nina.chepovska@gmail.com>
2016-05-03 12:47:51 -0700

    Added documentation to Member class

--- app/src/main/java/tcss450/uw/edu/chorewizard/model/Member.java
+++ app/src/main/java/tcss450/uw/edu/chorewizard/model/Member.java
@@ -8,37 +8,39 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * Created by alice on 5/3/2016.
+ * A class which represents a household member that will participate
+ * in getting chores done.
+ *
+ * @version May 3, 2016
  */
 public class Member implements Serializable {
-    private static final long serialVersionUID = 0L;
 
+    /** The household members name. */
     private String name;
+
+    /** The household members phone number. */
     private String phone;
 
+    /** Constants to hold the names of the class fields. */
     public static final String NAME = "name", PHONE = "phone";
 
+    /** Serialization id. */
+    private static final long serialVersionUID = 0L;
+
+    /**
+     * An empty class constructor.
+     */
     public Member() {
+
     }
 
+    /**
+     * Constructor used to initialize the class fields.
+     * @param name the members name.
+     * @param phone the members phone number.
+     */
     public Member(final String name, final String phone) {
         this.name = name;
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
     }
 
@@ -67,6 +69,23 @@ public class Member implements Serializable {
         return reason;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
     public String toString() {
         return name + " " + phone;
     }
